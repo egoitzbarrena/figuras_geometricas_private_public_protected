cf222075a6cd8aa8ccb4ff37be6885bd8e930d6d
egoitz <ebarrenalo12is@ikzubirimanteo.com>
2015-01-29 09:42:44 +0100

    private,public,protected

---
+++ figuras/src/Triangulo.java
@@ -0,0 +1,26 @@
+public class Triangulo extends FiguraGeometrica {
+
+	public Triangulo(){};
+	
+	public Triangulo(double base, double altura) {
+		setNombre("Triangulo");
+		setBase(base);
+		setAltura(altura);		
+	}
+
+	@Override public double area(){
+		double area = getBase()*getAltura()/2;
+		return area;
+	}
+	//Triángulo equilatero
+	@Override public double perimetro() {
+		double peri = getBase()*3;
+		return peri;
+	}
+	
+	public void mostrar() {
+	    super.mostrar();
+		System.out.println("\nEl area del triangulo es: " + area());
+		System.out.println("\nEl perimetro del triangulo es: " + perimetro());
+	}
+}
